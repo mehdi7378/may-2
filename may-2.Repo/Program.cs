337f728f7ac892a0280c79ea251a642cbeb4fe2a
m.tajodnin65@gmail.com <m.tajodini65@gmail.com>
2020-03-20 00:30:13 +0330

    init project

---
+++ may-2.Repo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace may_2.Repo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
